299afde9f06e19236026db206cc49f85df67d9bb
Leblanc <leblanc@cas-web-lp001.phy.ohio.edu>
2024-11-27 15:15:22 -0500

    Add a login button to the header.

--- header.php
+++ header.php
@@ -10,5 +10,6 @@ if (session_status() === PHP_SESSION_NONE) {
         <a href="logout.php" class="logout-button">Logout</a>
     <?php else: ?>
         <span>Welcome to the OUAL Training Information Portal</span>
+        <a href="login.php" class="logout-button">Login</a>
     <?php endif; ?>
 </div>
