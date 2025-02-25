a732ecd4e9dc6a86af914a37fef258dccd953d44
Leblanc <leblanc@cas-web-lp001.phy.ohio.edu>
2025-02-25 16:47:40 -0500

    Fix the login so that it returns to this page.

--- personnel_edit.php
+++ personnel_edit.php
@@ -35,7 +35,7 @@ $currentUrl = urlencode($_SERVER['REQUEST_URI']);
  * Redirects unauthorized users to the login page.
  */
 if (!isset($_SESSION['user_id']) || ($_SESSION['role_id'] < 1 || $_SESSION['role_id'] > 2)) {
-    header("Location: login.php");
+    header("Location: login.php?return=$currentUrl");
     exit();
 }
 
