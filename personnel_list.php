fde789452fb204b359ccec55bdbb0530f85688cc
Gregory Leblanc <leblanc@ohio.edu>
2026-07-24 16:58:36 -0400

    Another fix for operators with no email.

--- personnel_list.php
+++ personnel_list.php
@@ -116,9 +116,9 @@ $opertor_list = $mysqli->query(
              */
             $rowCounter = 0; // Unique ID for each row's email
             while ($res = mysqli_fetch_array($opertor_list)) {
-                $email = explode('@', $res['OperatorEmail']);
-                $user = $email[0];
-                $domain = $email[1];
+                $email = explode('@', $res['OperatorEmail'] ?? '');
+                $user = $email[0] ?? '';
+                $domain = $email[1] ?? '';
                 $rowId = "email-" . $rowCounter++; // Generate a unique ID
                 echo "<tr>";
                 echo "<td>" . htmlspecialchars($res['OperatorName']) . "</td>\n";
